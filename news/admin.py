9fe3dbd0b788fe846f886f702660eb4710536065
Yuliia Hladyshkevych <yulia.hladyshkevych@gmail.com>
2024-06-07 19:01:57 +0300

    Register NewsArticle and Trend models in Django admin

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from news.models import NewsArticle, Trend
+
+
+@admin.register(NewsArticle)
+class NewsArticleAdmin(admin.ModelAdmin):
+    ordering = ["-publication_date"]
+
+
+@admin.register(Trend)
+class TrendAdmin(admin.ModelAdmin):
+    ordering = ["-date"]
