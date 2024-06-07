47b23f3264d225b9e11cfb8aef04145c78685dd9
Yuliia Hladyshkevych <yulia.hladyshkevych@gmail.com>
2024-06-07 18:58:53 +0300

    Add models for news trends and articles

--- news/models.py
+++ news/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Trend(models.Model):
+    """Model for a news trend."""
+
+    keyword = models.CharField("Keyword", max_length=255)
+    date = models.DateField("Date")
+
+    class Meta:
+        unique_together = ("keyword", "date")
+
+    def __str__(self):
+        return self.keyword
+
+
+class NewsArticle(models.Model):
+    """Model for an individual news article."""
+
+    title = models.CharField("Title", max_length=1000)
+    link = models.URLField("Article URL", max_length=200, unique=True)
+    publication_date = models.DateTimeField("Publication Date", null=True, blank=True)
+    sentiment_score = models.FloatField("Sentiment Score", null=True, blank=True)
+    trends = models.ManyToManyField(Trend, related_name="news_articles")
+
+    def __str__(self):
+        return f"{self.title} / {self.publication_date:%Y-%m-%d}"
+
+    class Meta:
+        verbose_name = "News article"
+        verbose_name_plural = "News articles"
